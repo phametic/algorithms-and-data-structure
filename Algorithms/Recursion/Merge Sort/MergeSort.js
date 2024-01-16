314135fdd76fb197b07c98d79ae610a584315fd4
phametic <ram.pham@gmail.com>
2024-01-15 22:34:26 -0500

    WIP MergeSort

---
+++ Algorithms/Recursion/Merge Sort/MergeSort.js
@@ -0,0 +1,14 @@
+function merge() {
+    
+}
+function mergeSort(arr, low, high) {
+    
+
+    if (low < high) {
+        var mid = (low + high) / 2
+    }
+}
+
+var testInput = [3, 2, 1, 13, 8, 5, 0, 1];
+
+console.log(mergeSort(testInput, testInput[0], testInput[testInput.length]))
